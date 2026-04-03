0c8578aeb990f72cd8eb27fc45b3543609f3a73d
henoctshimanga <henoctshimanga79@gmail.com>
2026-04-03 08:18:44 -0400

    updated terminal class to instantiate services at once

--- src/presentation/terminal.py
+++ src/presentation/terminal.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
 
+from service.course_service import CourseService
+from service.professor_service import ProfessorService
 from service.student_service import StudentService
 
 # To prevent circular imports
@@ -9,15 +11,19 @@ if TYPE_CHECKING:
 
 
 class Terminal:
-    def __init__(self, student_service: StudentService):
+    def __init__(self):
         from presentation.menu import MainMenu
+
+         # Each service is instantiated once and shared across all menus via self.terminal.
+        # This ensures all menus read and write to the same data.
+        self.student_service = StudentService()
+        self.professor_service = ProfessorService()
+        self.courses_service = CourseService()
         
         #Start with the Main Menu
         self.current_menu = MainMenu(self)
 
         self.running = True
-
-        self.student_service = student_service
 
 
     def navigateToMenu(self, menu: Menu):
