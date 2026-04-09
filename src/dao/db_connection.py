e620a1228db65279701f8a669ab685cb270be5cd
henoctshimanga <henoctshimanga79@gmail.com>
2026-04-09 06:01:53 -0400

    created db_connection class

---
+++ src/dao/db_connection.py
@@ -0,0 +1,36 @@
+import os
+import mysql.connector
+
+from mysql.connector import Error
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+
+class DBConnection:
+
+    @staticmethod
+    def get_connection():
+        try:
+            return mysql.connector.connect(
+            host=os.getenv("DB_HOST"),
+            port=os.getenv("PORT"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASS"),
+            database=os.getenv("DB_NAME")
+            )
+        except Error as e:
+            print(f"Error connecting to database: {e}")
+            return None
+    
+
+#     def test_connection() -> None:
+#         with DBConnection.get_connection() as conn:
+#             cursor = conn.cursor(dictionary=True)
+
+#             sql = "CREATE TABLE test_table (id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255))"
+
+#             cursor.execute(sql)
+
+# DBConnection.test_connection()
